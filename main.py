0c72fb7e3fa43d66e936c2772919e1071e1f7616
Keerthana <keerthanareshaboina@gmail.com>
2024-12-16 23:27:43 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from multilingual_pdf2text.pdf2text import PDF2Text
+from multilingual_pdf2text.models.document_model.document import Document
+import logging
+logging.basicConfig(level=logging.INFO)
+    
+## create document for extraction with configurations
+pdf_document = Document(
+    document_path=r" ",  #add path of pdf
+    language="eng"
+    )
+pdf2text = PDF2Text(document=pdf_document)
+content = pdf2text.extract()
+length = len(content)
+text = ''
+# print(content)
+for i in range(length):
+    matter = content[i]["text"]
+            
+    text+=matter
+    text = str(text).replace("[","")
+    text = str(text).replace("]","")
+    text = str(text).replace("\\n","")
+    text = str(text).replace("\'", "")
+        
+    #open text file
+    text_file = open(r"C:\Users\output.pdf", "w", encoding="utf-8")
+        
+        #write string to file
+    text_file.write(text)
+    
+ 
+#close file
+    text_file.close()
